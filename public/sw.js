8b8c016f939c5a1db8f43915a0afa867e5a555d0
billyphosly4 <billyphosly4@gmail.com>
2026-09-04 12:46:55 +0300

    update

--- public/sw.js
+++ public/sw.js
@@ -11,8 +11,11 @@ const PRECACHE_ASSETS = [
   '/manifest.json',
   '/favicon.png',
   '/logo.png',
+  '/apple-touch-icon.png',
   '/icon-192.png',
-  '/icon-512.png'
+  '/icon-192-maskable.png',
+  '/icon-512.png',
+  '/icon-512-maskable.png'
 ];
 
 // Install Event — Pre-cache App Shell Assets
